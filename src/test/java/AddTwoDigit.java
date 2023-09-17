b6dd8a91a8be99dae71b8a6002f44982046ab939
Baljeet <baljeetsinghdeol22@gmail.com>
2023-09-17 07:55:03 +0530

    add the frist message

---
+++ src/test/java/AddTwoDigit.java
@@ -0,0 +1,13 @@
+public class AddTwoDigit {
+    int i =10;
+    int j = 22;
+    int c = i+j;
+    void show(){
+        System.out.println(this.c);
+    }
+
+    public static void main(String[] args) {
+        AddTwoDigit addTwoDigit = new AddTwoDigit();
+        addTwoDigit.show();
+    }
+}
